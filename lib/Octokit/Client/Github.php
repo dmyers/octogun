a9aa27a6d3ab080b87249c4a08809ae9e6ffd492
Derek Myers <arcticpro@gmail.com>
2013-12-08 23:32:43 -0600

    Added new github client

--- lib/Octokit/Client/Github.php
+++ lib/Octokit/Client/Github.php
@@ -6,4 +6,17 @@ use Octokit\Api;
 
 class Github extends Api
 {
+    /**
+     * Get meta information about GitHub.com, the service.
+     * 
+     * @see http://developer.github.com/v3/meta/
+     * 
+     * @param array $options Optional options.
+     * 
+     * @return array Array with meta information.
+     */
+    public function githubMeta(array $options = array())
+    {
+        return $this->request()->get('meta', $options);
+    }
 }
