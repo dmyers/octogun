21af3bdce4f45a4703b78dc2f660cd286949471e
Derek Myers <arcticpro@gmail.com>
2013-12-23 22:43:23 -0600

    Added emoji client test

---
+++ test/Octokit/Test/Client/EmojisTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Octokit\Test\Client;
+
+use Octokit\Client;
+
+class EmojisTest extends \PHPUnit_Framework_TestCase
+{
+    public $client;
+    
+    public function setUp()
+    {
+        $this->client = new Client();
+    }
+    
+    public function tearDown()
+    {
+        $this->configuration()->reset();
+    }
+    
+    public function configuration()
+    {
+        return $this->client->configuration();
+    }
+    
+    public function request()
+    {
+        return $this->client->request();
+    }
+    
+    public function emojis()
+    {
+        return $this->client->emojis();
+    }
+    
+    public function testGithubMeta()
+    {
+        $this->request()->setFixture('emojis');
+        
+        $emojis = $this->emojis()->emojis();
+        
+        $this->assertEquals($emojis['metal'], 'https://a248.e.akamai.net/assets.github.com/images/icons/emoji/metal.png?v5');
+    }
+}
