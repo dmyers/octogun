5bce3d66437282d27b19df245a13900207901a08
Derek Myers <arcticpro@gmail.com>
2013-12-23 22:42:23 -0600

    Added new emojis client

--- lib/Octokit/Client/Emojis.php
+++ lib/Octokit/Client/Emojis.php
@@ -6,4 +6,15 @@ use Octokit\Api;
 
 class Emojis extends Api
 {
+    /**
+     * List all emojis used on GitHub.
+     * 
+     * @see http://developer.github.com/v3/emojis/#emojis
+     * 
+     * @return array A list of all emojis on GitHub.
+     */
+    public function emojis()
+    {
+        return $this->request()->get('emojis');
+    }
 }
