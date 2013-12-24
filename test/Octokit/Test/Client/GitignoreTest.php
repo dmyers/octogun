08bb8bf94281efdac5a0b0da32a363a0ce48e07d
Derek Myers <arcticpro@gmail.com>
2013-12-24 03:00:01 -0600

    Added gitignore client test

---
+++ test/Octokit/Test/Client/GitignoreTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Octokit\Test\Client;
+
+use Octokit\Client;
+
+class GitignoreTest extends \PHPUnit_Framework_TestCase
+{
+    public $client;
+    
+    public function setUp()
+    {
+        $this->client = new Client();
+    }
+    
+    public function tearDown()
+    {
+        $this->configuration()->reset();
+    }
+    
+    public function configuration()
+    {
+        return $this->client->configuration();
+    }
+    
+    public function request()
+    {
+        return $this->client->request();
+    }
+    
+    public function gitignore()
+    {
+        return $this->client->gitignore();
+    }
+    
+    public function testGitignoreTemplates()
+    {
+        $this->request()->setFixture('gitignore_templates');
+        
+        $templates = $this->gitignore()->gitignoreTemplates();
+        
+        $this->assertEquals($templates[0], 'Actionscript');
+    }
+    
+    public function testGitignoreTemplate()
+    {
+        $this->request()->setFixture('gitignore_template_ruby');
+        
+        $template = $this->gitignore()->gitignoreTemplate('Ruby');
+        
+        $this->assertEquals($template['name'], 'Ruby');
+        $this->assertTrue(strpos($template['source'], "*.gem\n") !== false);
+    }
+}
