fff666501ac4c43c009e1959e4df8f49f51a2180
Derek Myers <arcticpro@gmail.com>
2013-12-25 00:37:58 -0600

    Added new markdown client

--- lib/Octokit/Client/Markdown.php
+++ lib/Octokit/Client/Markdown.php
@@ -6,4 +6,21 @@ use Octokit\Api;
 
 class Markdown extends Api
 {
+    /**
+     * Render an arbitrary Markdown document.
+     * 
+     * @see http://developer.github.com/v3/repos/markdown/
+     *
+     * @param string $text    Markdown source.
+     * @param array  $options Optional options.
+     *
+     * @return string HTML renderization.
+     */
+    public function markdown($text, array $options = array())
+    {
+        $options['text'] = $text;
+        $options['accept'] = 'application/vnd.github.raw';
+        
+        return $this->request()->post('markdown', $options);
+    }
 }
