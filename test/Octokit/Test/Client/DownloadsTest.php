cc51b2215d76b9df0db0e4051a66321c025eca6d
Derek Myers <arcticpro@gmail.com>
2014-01-03 04:21:40 -0600

    Added download client test

---
+++ test/Octokit/Test/Client/DownloadsTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Octokit\Test\Client;
+
+use Octokit\Client;
+
+class DownloadsTest extends \PHPUnit_Framework_TestCase
+{
+    public $client;
+    
+    public function setUp()
+    {
+        $this->client = new Client(array('login' => 'sferik'));
+    }
+    
+    public function tearDown()
+    {
+        $this->configuration()->reset();
+    }
+    
+    public function configuration()
+    {
+        return $this->client->configuration();
+    }
+    
+    public function request()
+    {
+        return $this->client->request();
+    }
+    
+    public function downloads()
+    {
+        return $this->client->downloads();
+    }
+    
+    public function testDownloads()
+    {
+        $this->request()->setFixture('downloads');
+        
+        $downloads = $this->downloads()->downloads('github/hubot');
+        
+        $this->assertEquals($downloads[0]['description'], 'Robawt');
+    }
+    
+    public function testDownload()
+    {
+        $this->request()->setFixture('download');
+        
+        $download = $this->downloads()->download('github/hubot', 165347);
+        
+        $this->assertEquals($download['id'], 165347);
+        $this->assertEquals($download['name'], 'hubot-2.1.0.tar.gz');
+    }
+    
+    public function testDeleteDownload()
+    {
+        $this->request()->setFixture(array(
+            'status' => 204,
+            'body'   => '',
+        ));
+        
+        $result = $this->downloads()->deleteDownload('github/hubot', 165347);
+        
+        $this->assertTrue($result);
+    }
+}
