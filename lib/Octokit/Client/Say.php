cd4bd42426117a4ddfea1d1b4d94a4fe6ca4f811
Derek Myers <arcticpro@gmail.com>
2014-01-04 05:32:06 -0600

    Added docblock for say method in say client

--- lib/Octokit/Client/Say.php
+++ lib/Octokit/Client/Say.php
@@ -9,7 +9,16 @@ class Say extends Api
     public $aliases = array(
         'octocat' => 'say',
     );
-
+    
+    /**
+     * Return a nifty ASCII Octocat with GitHub wisdom
+     * or your own.
+     *
+     * @param string $text    The text to display.
+     * @param array  $options Optional options.
+     *
+     * @return string
+     */
     public function say($text = null, array $options = array())
     {
         if (!empty($text)) {
