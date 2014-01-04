587ea8ae5bf47587d70aa5d9496aa3d845a7802f
Derek Myers <arcticpro@gmail.com>
2014-01-04 00:04:01 -0600

    Added new say client

--- lib/Octokit/Client/Say.php
+++ lib/Octokit/Client/Say.php
@@ -6,4 +6,16 @@ use Octokit\Api;
 
 class Say extends Api
 {
+    public $aliases = array(
+        'octocat' => 'say',
+    );
+
+    public function say($text = null, array $options = array())
+    {
+        if (!empty($text)) {
+            $options['s'] = $text;
+        }
+        
+        return $this->request()->get('octocat', $options);
+    }
 }
